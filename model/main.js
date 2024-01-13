79748218dd8c5bb29d011f83e898dbebf20509e7
zee02 <zefrancisco2002@gmail.com>
2024-01-13 02:40:21 +0000

    Done

--- model/main.js
+++ model/main.js
@@ -5,10 +5,11 @@ import * as THREE from "three";
 let cena = new THREE.Scene();
 let carregador = new GLTFLoader();
 let mixer;
-let actionOpenDrawerLeft,
-  actionOpenDrawerRight,
-  actionOpenDoorLeft,
-  actionOpenDoorRight;
+
+let gaveta_direita_action = 0;
+let gaveta_esquerda_action = 0;
+let porta_direita_action = 0;
+let porta_esquerda_action = 0;
 
 // Criar um plano
 let geometry = new THREE.PlaneGeometry(10, 20);
@@ -24,7 +25,7 @@ let camara = new THREE.PerspectiveCamera(
   0.10,
   2000
 );
-camara.position.set(0, 8, 3);
+camara.position.set(0, 5, 10);
 
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize(900, 800);
@@ -39,20 +40,39 @@ document.addEventListener("DOMContentLoaded", function () {
       imageContainer.style.display = 'none';
     }
   }
-  
+
   function loadModel(modelURL) {
-    carregador.load(modelURL, function(gltf) {
+    carregador.load(modelURL, function (gltf) {
       cena.add(gltf.scene);
+      mixer = new THREE.AnimationMixer(gltf.scene);
       modelContainer1.appendChild(renderer.domElement);
-      cena.background = new THREE.Color(0xffffff); 
+      cena.background = new THREE.Color(0xffffff);
+
+      const gaveta_direita = THREE.AnimationClip.findByName(gltf.animations, "Drawer_RightOpen");
+      const gaveta_esquerda = THREE.AnimationClip.findByName(gltf.animations, "Drawer_LeftOpen");
+      const porta_direita = THREE.AnimationClip.findByName(gltf.animations, "Door_LeftOpen");
+      const porta_esquerda = THREE.AnimationClip.findByName(gltf.animations, "Door_RightOpen");
+
+      gaveta_direita_action = mixer.clipAction(gaveta_direita);
+      gaveta_esquerda_action = mixer.clipAction(gaveta_esquerda);
+      porta_direita_action = mixer.clipAction(porta_direita);
+      porta_esquerda_action = mixer.clipAction(porta_esquerda);
+
+      porta_direita_action.setLoop(THREE.LoopOnce);
+      porta_esquerda_action.setLoop(THREE.LoopOnce);
+      gaveta_direita_action.setLoop(THREE.LoopOnce);
+      gaveta_esquerda_action.setLoop(THREE.LoopOnce);
     });
   }
-  
+
   function loadModelByOption(option) {
     hideImages();
+
     modelContainer1.style.display = 'block';
     modelContainer1.innerHTML = '';
-    
+
+
+
     switch (option) {
       case 'default':
         loadModel('/model/modelo/vintageDesk.gltf');
@@ -67,25 +87,25 @@ document.addEventListener("DOMContentLoaded", function () {
         break;
     }
   }
-  
+
   const btnDefault = document.getElementById("default");
   const btnOption1 = document.getElementById("option1");
   const btnOption2 = document.getElementById("option2");
-  
+
   if (btnDefault) {
-    btnDefault.addEventListener('click', function() {
+    btnDefault.addEventListener('click', function () {
       loadModelByOption('default');
     });
   }
-  
+
   if (btnOption1) {
-    btnOption1.addEventListener('click', function() {
+    btnOption1.addEventListener('click', function () {
       loadModelByOption('option1');
     });
   }
-  
+
   if (btnOption2) {
-    btnOption2.addEventListener('click', function() {
+    btnOption2.addEventListener('click', function () {
       loadModelByOption('option2');
     });
   }
@@ -114,64 +134,163 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
 
-let abrindo = {
-  Gaveta_L: false,
-  Gaveta_R: false,
-  Porta_L: false,
-  Porta_R: false,
-}; // Armazena o estado de abertura dos objetos
+  let abrindo = {
+    Gaveta_L: false,
+    Gaveta_R: false,
+    Porta_L: false,
+    Porta_R: false,
+  }; // Armazena o estado de abertura dos objetos
 
-function toggleAnimationState(action, intersects, objeto) {
-  if (intersects.length > 0 && action) {
-    const objetoAberto = abrindo[objeto];
+  function toggleAnimationState(action, intersects, objeto) {
+    if (intersects.length > 0 && action) {
+      const objetoAberto = abrindo[objeto];
 
-    if (!objetoAberto) {
-      action.paused = false;
-      action.reset();
-      action.clampWhenFinished = true;
-      action.loop = THREE.LoopOnce;
-      action.timeScale = 1;
-      action.play();
-      abrindo[objeto] = true;
+      if (!objetoAberto) {
+        action.paused = false;
+        action.reset();
+        action.clampWhenFinished = true;
+        action.loop = THREE.LoopOnce;
+        action.timeScale = 1;
+        action.play();
+        abrindo[objeto] = true;
 
-      if (!action._listeners.finished) {
-        action._listeners.finished = function () {
-          abrindo[objeto] = false;
-        };
+        if (!action._listeners.finished) {
+          action._listeners.finished = function () {
+            abrindo[objeto] = false;
+          };
+        }
+      } else {
+        action.paused = false;
+        action.timeScale = -1;
+        action.play();
+        abrindo[objeto] = false;
       }
-    } else {
-      action.paused = false;
-      action.timeScale = -1;
-      action.play();
-      abrindo[objeto] = false;
     }
   }
-}
 
-function checkAnimationStatus() {
-  for (const objeto in abrindo) {
-    if (abrindo[objeto] && actionOpenDrawerLeft.paused) {
-      // Se a animação do objeto em processo está pausada, terminamos o processo
-      abrindo[objeto] = false;
+  const fechar_portas = document.getElementById('fechar-portas');
+  fechar_portas.addEventListener('click', () => {
+    // Adicionar um evento de termino de animação para cada ação
+    porta_direita_action.clampWhenFinished = true;
+    porta_direita_action.reset();
+    porta_direita_action.stop();
+
+    porta_esquerda_action.clampWhenFinished = true;
+    porta_esquerda_action.reset();
+    porta_esquerda_action.stop();
+    // Verificar se a animação foi encontrada antes de acessar a propriedade isRunning
+    if (porta_direita_action && porta_esquerda_action) {
+      const isPortaDireitaRunning = porta_direita_action.isRunning();
+      const isPortaEsquerdaRunning = porta_esquerda_action.isRunning();
+
+      // Reiniciar e reproduzir ambas as animações se não estiverem em execução
+      if (!isPortaDireitaRunning && !isPortaEsquerdaRunning) {
+        porta_direita_action.reset().play() - 1;
+        porta_esquerda_action.reset().play() - 1;
+        porta_direita_action.stop();
+        porta_esquerda_action.stop();
+      }
+    }
+  });
+  const portas = document.getElementById('portas');
+  portas.addEventListener('click', () => {
+    // Adicionar um evento de termino de animação para cada ação
+    porta_direita_action.clampWhenFinished = true;
+    porta_direita_action.reset();
+    porta_direita_action.stop();
+
+    porta_esquerda_action.clampWhenFinished = true;
+    porta_esquerda_action.reset();
+    porta_esquerda_action.stop();
+    // Verificar se a animação foi encontrada antes de acessar a propriedade isRunning
+    if (porta_direita_action && porta_esquerda_action) {
+      const isPortaDireitaRunning = porta_direita_action.isRunning();
+      const isPortaEsquerdaRunning = porta_esquerda_action.isRunning();
+
+      // Reiniciar e reproduzir ambas as animações se não estiverem em execução
+      if (!isPortaDireitaRunning && !isPortaEsquerdaRunning) {
+        porta_direita_action.reset().play();
+        porta_esquerda_action.reset().play();
+      }
+    }
+  });
+
+  const fechar_gavetas = document.getElementById('fechar-gavetas');
+  fechar_gavetas.addEventListener('click', () => {
+    // Adicionar um evento de termino de animação para cada ação
+    gaveta_direita_action.clampWhenFinished = true;
+    gaveta_direita_action.reset();
+    gaveta_direita_action.stop();
+
+    gaveta_esquerda_action.clampWhenFinished = true;
+    gaveta_esquerda_action.reset();
+    gaveta_esquerda_action.stop();
+    // Verificar se a animação foi encontrada antes de acessar a propriedade isRunning
+    if (gaveta_direita_action && gaveta_esquerda_action) {
+      const isGavetaDireitaRunning = gaveta_direita_action.isRunning();
+      const isGavetaEsquerdaRunning = gaveta_esquerda_action.isRunning();
+
+      // Reiniciar e reproduzir ambas as animações se não estiverem em execução
+      if (!isGavetaDireitaRunning && !isGavetaEsquerdaRunning) {
+        gaveta_direita_action.reset().play() - 1;
+        gaveta_esquerda_action.reset().play() - 1;
+        gaveta_direita_action.stop();
+        gaveta_esquerda_action.stop();
+      }
+    }
+  });
+
+  const gavetas = document.getElementById('gavetas');
+  gavetas.addEventListener('click', () => {
+    // Adicionar um evento de termino de animação para cada ação
+    gaveta_direita_action.clampWhenFinished = true;
+    gaveta_direita_action.reset();
+    gaveta_direita_action.stop();
+
+    gaveta_esquerda_action.clampWhenFinished = true;
+    gaveta_esquerda_action.reset();
+    gaveta_esquerda_action.stop();
+    // Verificar se a animação foi encontrada antes de acessar a propriedade isRunning
+    if (gaveta_direita_action && gaveta_esquerda_action) {
+      const isGavetaDireitaRunning = gaveta_direita_action.isRunning();
+      const isGavetaEsquerdaRunning = gaveta_esquerda_action.isRunning();
+
+      // Reiniciar e reproduzir ambas as animações se não estiverem em execução
+      if (!isGavetaDireitaRunning && !isGavetaEsquerdaRunning) {
+        gaveta_direita_action.reset().play();
+        gaveta_esquerda_action.reset().play();
+      }
+    }
+  });
+
+
+
+  function checkAnimationStatus() {
+    for (const objeto in abrindo) {
+      if (abrindo[objeto] && actionOpenDrawerLeft.paused) {
+        // Se a animação do objeto em processo está pausada, terminamos o processo
+        abrindo[objeto] = false;
+      }
     }
   }
-}
 
-function luzes(cena) {
-  const luzAmbiente = new THREE.AmbientLight("lightgreen");
-  cena.add(luzAmbiente);
+  function luzes(cena) {
+    const luzAmbiente = new THREE.AmbientLight("lightgreen");
+    cena.add(luzAmbiente);
 
-  const luzPonto = new THREE.PointLight("white");
-  luzPonto.position.set(0, 2, 2);
-  luzPonto.intensity = 5;
-  cena.add(luzPonto);
+    const luzPonto = new THREE.PointLight("white");
+    luzPonto.position.set(0, 2, 2);
+    luzPonto.intensity = 5;
+    cena.add(luzPonto);
 
-  const luzDirecional = new THREE.DirectionalLight("white");
-  luzDirecional.position.set(3, 2, 0);
-  luzDirecional.intensity = 5;
-  cena.add(luzDirecional);
-}
+    const luzDirecional = new THREE.DirectionalLight("white");
+    luzDirecional.position.set(3, 2, 0);
+    luzDirecional.intensity = 5;
+    cena.add(luzDirecional);
+  }
 
-luzes(cena);
-animar();
+  luzes(cena);
+  animar();
 });
+
+
--- model/main2.js
+++ model/main2.js
@@ -5,50 +5,19 @@ import * as THREE from "three";
 let cena = new THREE.Scene();
 let carregador = new GLTFLoader();
 let mixer;
-let actionOpenDrawerLeft,
-  actionOpenDrawerRight,
-  actionOpenDoorLeft,
-  actionOpenDoorRight;
+
+let gaveta_direita_action = 0;
+let gaveta_esquerda_action = 0;
+let porta_direita_action = 0;
+let porta_esquerda_action = 0;
 
 // Criar um plano
-let geometry = new THREE.PlaneGeometry(10, 10);
-let material = new THREE.MeshBasicMaterial({ transparent: true, opacity: 0   });
+let geometry = new THREE.PlaneGeometry(10, 20);
+let material = new THREE.MeshBasicMaterial({ transparent: true, opacity: 0 });
 let plano = new THREE.Mesh(geometry, material);
 
-// Adicione o plano à cena
+// Adicionar o plano à cena
 cena.add(plano);
-
-carregador.load("/model/ModeloGray/vintageDesk.gltf", function (gltf) {
-  cena.add(gltf.scene);
-
-  let objeto3D = gltf.scene;
-  cena.background = new THREE.Color(0xffffff); 
-
-  cena.add(objeto3D); // Adiciona o modelo à cena
-  const clipeOpenDrawerLeft = THREE.AnimationClip.findByName(
-    gltf.animations,
-    "Drawer_RightOpen"
-  );
-  const clipeOpenDrawerRight = THREE.AnimationClip.findByName(
-    gltf.animations,
-    "Drawer_LeftOpen"
-  );
-  const clipeOpenDoorRight = THREE.AnimationClip.findByName(
-    gltf.animations,
-    "Door_LeftOpen"
-  );
-  const clipeOpenDoorLeft = THREE.AnimationClip.findByName(
-    gltf.animations,
-    "Door_RightOpen"
-  );
-
-  // Criar mixer e controladores de animação
-  mixer = new THREE.AnimationMixer(gltf.scene);
-  actionOpenDrawerLeft = mixer.clipAction(clipeOpenDrawerLeft);
-  actionOpenDrawerRight = mixer.clipAction(clipeOpenDrawerRight);
-  actionOpenDoorLeft = mixer.clipAction(clipeOpenDoorLeft);
-  actionOpenDoorRight = mixer.clipAction(clipeOpenDoorRight);
-});
 
 let camara = new THREE.PerspectiveCamera(
   20,
@@ -56,142 +25,272 @@ let camara = new THREE.PerspectiveCamera(
   0.10,
   2000
 );
-camara.position.set(2, 4, 9);
-
+camara.position.set(0, 5, 10);
 
 let renderer = new THREE.WebGLRenderer();
-renderer.setSize(1000, 800);
+renderer.setSize(900, 800);
+
 
 document.addEventListener("DOMContentLoaded", function () {
-  const btnOption1 = document.getElementById("option1");
   const modelContainer1 = document.getElementById("modelContainer1");
 
-  if (btnOption1) {
-    btnOption1.addEventListener("click", function () {
-      // Esconde as imagens
-      const image1 = document.getElementById("imageContainer");
+  function hideImages() {
+    const imageContainer = document.getElementById('imageContainer');
+    if (imageContainer) {
+      imageContainer.style.display = 'none';
+    }
+  }
 
-      if (image1) {
-        image1.style.display = "none";
-      }
-      modelContainer1.style.display = "block";
+  function loadModel(modelURL) {
+    carregador.load(modelURL, function (gltf) {
+      cena.add(gltf.scene);
+      mixer = new THREE.AnimationMixer(gltf.scene);
       modelContainer1.appendChild(renderer.domElement);
+      cena.background = new THREE.Color(0xffffff);
+
+      const gaveta_direita = THREE.AnimationClip.findByName(gltf.animations, "Drawer_RightOpen");
+      const gaveta_esquerda = THREE.AnimationClip.findByName(gltf.animations, "Drawer_LeftOpen");
+      const porta_direita = THREE.AnimationClip.findByName(gltf.animations, "Door_LeftOpen");
+      const porta_esquerda = THREE.AnimationClip.findByName(gltf.animations, "Door_RightOpen");
+
+      gaveta_direita_action = mixer.clipAction(gaveta_direita);
+      gaveta_esquerda_action = mixer.clipAction(gaveta_esquerda);
+      porta_direita_action = mixer.clipAction(porta_direita);
+      porta_esquerda_action = mixer.clipAction(porta_esquerda);
+
+      porta_direita_action.setLoop(THREE.LoopOnce);
+      porta_esquerda_action.setLoop(THREE.LoopOnce);
+      gaveta_direita_action.setLoop(THREE.LoopOnce);
+      gaveta_esquerda_action.setLoop(THREE.LoopOnce);
     });
   }
 
-});
-new OrbitControls(camara, renderer.domElement);
+  function loadModelByOption(option) {
+    hideImages();
 
-let delta = 0;
-let relogio = new THREE.Clock();
-let latencia_minima = 1 / 60;
+    modelContainer1.style.display = 'block';
+    modelContainer1.innerHTML = '';
 
-function animar() {
-  requestAnimationFrame(animar);
-  delta += relogio.getDelta();
 
-  if (delta < latencia_minima) return;
 
-  if (mixer) {
-    mixer.update(latencia_minima); // Atualizar o mixer
-    checkAnimationStatus(); // Verificar o status das animações em processo
+    switch (option) {
+      case 'default':
+        loadModel('/model/modelo/vintageDesk.gltf');
+        break;
+      case 'option1':
+        loadModel('/model/ModeloBrown/vintageDesk.gltf');
+        break;
+      case 'option2':
+        loadModel('/model/ModeloGray/vintageDesk.gltf');
+        break;
+      default:
+        break;
+    }
   }
 
-  renderer.render(cena, camara);
-  delta = delta % latencia_minima;
-}
+  const btnDefault = document.getElementById("default");
+  const btnOption1 = document.getElementById("option1");
+  const btnOption2 = document.getElementById("option2");
 
-let abrindo = {
-  Gaveta_L: false,
-  Gaveta_R: false,
-  Porta_L: false,
-  Porta_R: false,
-}; // Armazena o estado de abertura dos objetos
+  if (btnDefault) {
+    btnDefault.addEventListener('click', function () {
+      loadModelByOption('default');
+    });
+  }
 
-function toggleAnimationState(action, intersects, objeto) {
-  if (intersects.length > 0 && action) {
-    const objetoAberto = abrindo[objeto];
+  if (btnOption1) {
+    btnOption1.addEventListener('click', function () {
+      loadModelByOption('option1');
+    });
+  }
 
-    if (!objetoAberto) {
-      action.paused = false;
-      action.reset();
-      action.clampWhenFinished = true;
-      action.loop = THREE.LoopOnce;
-      action.timeScale = 1;
-      action.play();
-      abrindo[objeto] = true;
+  if (btnOption2) {
+    btnOption2.addEventListener('click', function () {
+      loadModelByOption('option2');
+    });
+  }
 
-      if (!action._listeners.finished) {
-        action._listeners.finished = function () {
-          abrindo[objeto] = false;
-        };
+
+
+  new OrbitControls(camara, renderer.domElement);
+
+  let delta = 0;
+  let relogio = new THREE.Clock();
+  let latencia_minima = 1 / 60;
+
+  function animar() {
+    requestAnimationFrame(animar);
+    delta += relogio.getDelta();
+
+    if (delta < latencia_minima) return;
+
+    if (mixer) {
+      mixer.update(latencia_minima);
+      checkAnimationStatus();
+    }
+
+    renderer.render(cena, camara);
+    delta = delta % latencia_minima;
+  }
+
+
+  let abrindo = {
+    Gaveta_L: false,
+    Gaveta_R: false,
+    Porta_L: false,
+    Porta_R: false,
+  }; // Armazena o estado de abertura dos objetos
+
+  function toggleAnimationState(action, intersects, objeto) {
+    if (intersects.length > 0 && action) {
+      const objetoAberto = abrindo[objeto];
+
+      if (!objetoAberto) {
+        action.paused = false;
+        action.reset();
+        action.clampWhenFinished = true;
+        action.loop = THREE.LoopOnce;
+        action.timeScale = 1;
+        action.play();
+        abrindo[objeto] = true;
+
+        if (!action._listeners.finished) {
+          action._listeners.finished = function () {
+            abrindo[objeto] = false;
+          };
+        }
+      } else {
+        action.paused = false;
+        action.timeScale = -1;
+        action.play();
+        abrindo[objeto] = false;
       }
-    } else {
-      action.paused = false;
-      action.timeScale = -1;
-      action.play();
-      abrindo[objeto] = false;
     }
   }
-}
 
-function checkAnimationStatus() {
-  for (const objeto in abrindo) {
-    if (abrindo[objeto] && actionOpenDrawerLeft.paused) {
-      // Se a animação do objeto em processo está pausada, terminamos o processo
-      abrindo[objeto] = false;
+  const fechar_portas = document.getElementById('fechar-portas');
+  fechar_portas.addEventListener('click', () => {
+    // Adicionar um evento de termino de animação para cada ação
+    porta_direita_action.clampWhenFinished = true;
+    porta_direita_action.reset();
+    porta_direita_action.stop();
+
+    porta_esquerda_action.clampWhenFinished = true;
+    porta_esquerda_action.reset();
+    porta_esquerda_action.stop();
+    // Verificar se a animação foi encontrada antes de acessar a propriedade isRunning
+    if (porta_direita_action && porta_esquerda_action) {
+      const isPortaDireitaRunning = porta_direita_action.isRunning();
+      const isPortaEsquerdaRunning = porta_esquerda_action.isRunning();
+
+      // Reiniciar e reproduzir ambas as animações se não estiverem em execução
+      if (!isPortaDireitaRunning && !isPortaEsquerdaRunning) {
+        porta_direita_action.reset().play() - 1;
+        porta_esquerda_action.reset().play() - 1;
+        porta_direita_action.stop();
+        porta_esquerda_action.stop();
+      }
+    }
+  });
+  const portas = document.getElementById('portas');
+  portas.addEventListener('click', () => {
+    // Adicionar um evento de termino de animação para cada ação
+    porta_direita_action.clampWhenFinished = true;
+    porta_direita_action.reset();
+    porta_direita_action.stop();
+
+    porta_esquerda_action.clampWhenFinished = true;
+    porta_esquerda_action.reset();
+    porta_esquerda_action.stop();
+    // Verificar se a animação foi encontrada antes de acessar a propriedade isRunning
+    if (porta_direita_action && porta_esquerda_action) {
+      const isPortaDireitaRunning = porta_direita_action.isRunning();
+      const isPortaEsquerdaRunning = porta_esquerda_action.isRunning();
+
+      // Reiniciar e reproduzir ambas as animações se não estiverem em execução
+      if (!isPortaDireitaRunning && !isPortaEsquerdaRunning) {
+        porta_direita_action.reset().play();
+        porta_esquerda_action.reset().play();
+      }
+    }
+  });
+
+  const fechar_gavetas = document.getElementById('fechar-gavetas');
+  fechar_gavetas.addEventListener('click', () => {
+    // Adicionar um evento de termino de animação para cada ação
+    gaveta_direita_action.clampWhenFinished = true;
+    gaveta_direita_action.reset();
+    gaveta_direita_action.stop();
+
+    gaveta_esquerda_action.clampWhenFinished = true;
+    gaveta_esquerda_action.reset();
+    gaveta_esquerda_action.stop();
+    // Verificar se a animação foi encontrada antes de acessar a propriedade isRunning
+    if (gaveta_direita_action && gaveta_esquerda_action) {
+      const isGavetaDireitaRunning = gaveta_direita_action.isRunning();
+      const isGavetaEsquerdaRunning = gaveta_esquerda_action.isRunning();
+
+      // Reiniciar e reproduzir ambas as animações se não estiverem em execução
+      if (!isGavetaDireitaRunning && !isGavetaEsquerdaRunning) {
+        gaveta_direita_action.reset().play() - 1;
+        gaveta_esquerda_action.reset().play() - 1;
+        gaveta_direita_action.stop();
+        gaveta_esquerda_action.stop();
+      }
+    }
+  });
+
+  const gavetas = document.getElementById('gavetas');
+  gavetas.addEventListener('click', () => {
+    // Adicionar um evento de termino de animação para cada ação
+    gaveta_direita_action.clampWhenFinished = true;
+    gaveta_direita_action.reset();
+    gaveta_direita_action.stop();
+
+    gaveta_esquerda_action.clampWhenFinished = true;
+    gaveta_esquerda_action.reset();
+    gaveta_esquerda_action.stop();
+    // Verificar se a animação foi encontrada antes de acessar a propriedade isRunning
+    if (gaveta_direita_action && gaveta_esquerda_action) {
+      const isGavetaDireitaRunning = gaveta_direita_action.isRunning();
+      const isGavetaEsquerdaRunning = gaveta_esquerda_action.isRunning();
+
+      // Reiniciar e reproduzir ambas as animações se não estiverem em execução
+      if (!isGavetaDireitaRunning && !isGavetaEsquerdaRunning) {
+        gaveta_direita_action.reset().play();
+        gaveta_esquerda_action.reset().play();
+      }
+    }
+  });
+
+
+
+  function checkAnimationStatus() {
+    for (const objeto in abrindo) {
+      if (abrindo[objeto] && actionOpenDrawerLeft.paused) {
+        // Se a animação do objeto em processo está pausada, terminamos o processo
+        abrindo[objeto] = false;
+      }
     }
   }
-}
 
-function luzes(cena) {
-  const luzAmbiente = new THREE.AmbientLight("lightgreen");
-  cena.add(luzAmbiente);
+  function luzes(cena) {
+    const luzAmbiente = new THREE.AmbientLight("lightgreen");
+    cena.add(luzAmbiente);
 
-  const luzPonto = new THREE.PointLight("white");
-  luzPonto.position.set(0, 2, 2);
-  luzPonto.intensity = 15;
-  cena.add(luzPonto);
+    const luzPonto = new THREE.PointLight("white");
+    luzPonto.position.set(0, 2, 2);
+    luzPonto.intensity = 5;
+    cena.add(luzPonto);
 
-  const luzDirecional = new THREE.DirectionalLight("white");
-  luzDirecional.position.set(3, 2, 0);
-  luzDirecional.intensity = 30;
-  cena.add(luzDirecional);
-}
-
-var raycaster = new THREE.Raycaster();
-var mouse = new THREE.Vector2();
-let objetosAbertos = {}; // Armazena o estado dos objetos abertos
-
-window.addEventListener("click", onClick);
-
-function onClick(event) {
-  // Calcula as coordenadas normalizadas do mouse
-  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
-  // Atualiza o raycaster
-  raycaster.setFromCamera(mouse, camara);
-
-  // Encontrar os objetos desejados pelos nomes
-  const gaveta_L = cena.getObjectByName("Gaveta_L");
-  const gaveta_R = cena.getObjectByName("Gaveta_R");
-  const porta_L = cena.getObjectByName("Porta_L");
-  const porta_R = cena.getObjectByName("Porta_R");
-
-  // Verifica a interseção entre o raio e os objetos específicos
-  var intersectsGaveta_L = raycaster.intersectObject(gaveta_L, true);
-  var intersectsGaveta_R = raycaster.intersectObject(gaveta_R, true);
-  var intersectsPorta_L = raycaster.intersectObject(porta_L, true);
-  var intersectsPorta_R = raycaster.intersectObject(porta_R, true);
-
-  if (mixer) {
-    toggleAnimationState(actionOpenDrawerLeft, intersectsGaveta_L, "Gaveta_L");
-    toggleAnimationState(actionOpenDrawerRight, intersectsGaveta_R, "Gaveta_R");
-    toggleAnimationState(actionOpenDoorLeft, intersectsPorta_L, "Porta_L");
-    toggleAnimationState(actionOpenDoorRight, intersectsPorta_R, "Porta_R");
+    const luzDirecional = new THREE.DirectionalLight("white");
+    luzDirecional.position.set(3, 2, 0);
+    luzDirecional.intensity = 5;
+    cena.add(luzDirecional);
   }
-}
 
-luzes(cena);
-animar();
+  luzes(cena);
+  animar();
+});
+
+
